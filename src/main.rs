ba019fea01c9d9e75b158d48baac96d5bfeccded
James Hillyerd <james@hillyerd.com>
2022-05-25 20:28:58 -0700

    add Docker build with chef build deps caching

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, Axum World!" }));
+    let app = Router::new().route("/", get(|| async { "Chef says hello, Axum World!" }));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("Starting server on {}", addr);
