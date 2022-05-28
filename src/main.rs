8828b8cac7cc0767edaea6922bfd254a8babffe0
James Hillyerd <james@hillyerd.com>
2022-05-28 12:59:32 -0700

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/",
-        get(|| async { "You've found your waypoint, Axum." }),
-    );
+    let app = Router::new().route("/", get(|| async { "You've found your waypoint, Axum." }));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     println!("Starting server on {}", addr);
