28c13a3c9bd32a63305ec95492d469fcb7c4d634
James Hillyerd <james@hillyerd.com>
2022-05-28 18:23:23 -0700

    inject smtp transport

--- src/notify.rs
+++ src/notify.rs
@@ -1,11 +1,17 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::post, Json, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::post, Extension, Json, Router};
 use lettre::{AsyncSmtpTransport, Tokio1Executor};
 use serde::Deserialize;
 
 use crate::svc;
 
+type SmtpTx = AsyncSmtpTransport<Tokio1Executor>;
+
 pub fn router() -> Router {
-    Router::new().route("/mail", post(mail))
+    let mailer: SmtpTx = SmtpTx::builder_dangerous("nexus.home.arpa").build();
+
+    Router::new()
+        .route("/mail", post(mail))
+        .layer(Extension(mailer))
 }
 
 #[derive(Deserialize)]
@@ -14,9 +20,10 @@ struct Notification {
     body: String,
 }
 
-async fn mail(Json(note): Json<Notification>) -> impl IntoResponse {
-    let mailer = AsyncSmtpTransport::<Tokio1Executor>::builder_dangerous("nexus.home.arpa").build();
-
+async fn mail(
+    Extension(mailer): Extension<SmtpTx>,
+    Json(note): Json<Notification>,
+) -> impl IntoResponse {
     if let Err(err) = svc::mail::send_alert(&mailer, &note.subject, &note.body).await {
         tracing::error!("Failed to send mail: {err}");
         return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to send");
