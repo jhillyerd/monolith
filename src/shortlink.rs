e7aaade8192176d0d57a6c9dd7ddcb54a7aff168
James Hillyerd <james@hillyerd.com>
2023-09-05 15:44:48 -0700

    path /shortlink/go/:key now redirects to URL if found

--- src/shortlink.rs
+++ src/shortlink.rs
@@ -1,11 +1,19 @@
 use askama::Template;
-use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::{IntoResponse, Redirect},
+    routing::get,
+    Router,
+};
 use sqlx::FromRow;
 
 use crate::svc;
 
 pub fn router() -> Router<svc::State> {
-    Router::new().route("/list", get(list))
+    Router::new()
+        .route("/list", get(list))
+        .route("/go/:key", get(go))
 }
 
 #[derive(Debug, FromRow)]
@@ -18,6 +26,27 @@ struct Shortlink {
 #[template(path = "shortlink/list.html")]
 struct ListTemplate {
     links: Vec<Shortlink>,
+}
+
+async fn go(
+    Path(key): Path<String>,
+    State(state): State<svc::State>,
+) -> Result<impl IntoResponse, StatusCode> {
+    match sqlx::query_as::<_, Shortlink>("select name, url from shortlinks where name = $1")
+        .bind(&key)
+        .fetch_optional(&state.db)
+        .await
+    {
+        Ok(Some(link)) => Ok(Redirect::to(&link.url)),
+        Ok(None) => {
+            tracing::error!("No results for link named {:?}", key);
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+        Err(err) => {
+            tracing::error!("Failed to database: {err}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
 
 async fn list(State(state): State<svc::State>) -> Result<impl IntoResponse, StatusCode> {
